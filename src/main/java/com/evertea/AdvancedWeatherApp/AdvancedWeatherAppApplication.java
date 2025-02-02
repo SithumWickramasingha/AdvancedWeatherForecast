e0c583898cdfe8f98b8f64470d5582eaa8267a61
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-02-03 00:47:02 +0530

    first commit

---
+++ src/main/java/com/evertea/AdvancedWeatherApp/AdvancedWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.evertea.AdvancedWeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedWeatherAppApplication.class, args);
+	}
+
+}
