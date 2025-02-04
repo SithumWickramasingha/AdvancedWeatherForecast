e8c0f3693a44d65b944a45be367b286848349ba2
SithumWickramasingha <wickramasinghesithum96@gmail.com>
2025-02-04 22:07:45 +0530

    new changes

--- src/main/java/com/evertea/AdvancedWeatherApp/controller/WeatherController.java
+++ src/main/java/com/evertea/AdvancedWeatherApp/controller/WeatherController.java
@@ -1,7 +1,6 @@
 package com.evertea.AdvancedWeatherApp.controller;
 
-import com.evertea.AdvancedWeatherApp.model.Configuration;
-import com.evertea.AdvancedWeatherApp.main.Weather;
+import com.evertea.AdvancedWeatherApp.service.WeatherService;
 import com.evertea.AdvancedWeatherApp.model.WeatherData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController {
 
     @Autowired
-    private Weather weather;
+    private WeatherService weather;
 
     @PostMapping("/demo")
     public WeatherData getCity(@RequestBody WeatherData data){
